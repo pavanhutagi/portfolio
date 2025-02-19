7852e071427241da3fbfdd5af2a7af1f95016b07
Pavan Hutagi <pavanhutagi@gmail.com>
2025-02-19 16:04:28 +0530

    refactor: contact form color updates

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,7 +7,7 @@ export default function Button({ children, onClick }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="bg-background-dark dark:bg-background-light text-text-dark dark:text-text-light rounded-[15px] p-4"
+      className="bg-[#a82121] dark:bg-[#f34848] text-text-dark dark:text-text-light rounded-[15px] p-4"
     >
       {children}
     </button>
--- src/components/text-area.tsx
+++ src/components/text-area.tsx
@@ -16,7 +16,7 @@ export default function TextArea({
       placeholder={placeholder}
       value={value}
       onChange={(e) => onChange(e.target.value)}
-      className="bg-background-dark dark:bg-background-light rounded-[15px] p-4 max-h-[500px] min-h-[100px] outline-none focus:ring-2 focus:ring-primary-500 focus:border-primary-500"
+      className="bg-secondary-500 dark:bg-secondary-200 text-text-dark dark:text-text-light rounded-[15px] p-4 max-h-[500px] min-h-[100px] outline-none focus:ring-2 focus:ring-primary-500 focus:border-primary-500"
       rows={rows}
     />
   );
