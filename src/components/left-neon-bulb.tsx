05887e301bc3cdba98656e1d2d387901c91a1c4f
Pavan Hutagi <pavanhutagi@gmail.com>
2025-03-03 12:13:19 +0530

    refactor: updated the breakpoints for neon lights

--- src/components/left-neon-bulb.tsx
+++ src/components/left-neon-bulb.tsx
@@ -7,10 +7,10 @@ export default function LeftNeonBulb() {
 
   return (
     <div>
-      <div className="fixed inset-x-0 top-0 flex h-[20px] w-full justify-center z-50 sm:fixed sm:left-0 sm:top-1/2 sm:-translate-y-1/2 sm:h-auto sm:w-[20px]">
+      <div className="fixed inset-x-0 top-0 flex h-[20px] w-full justify-center z-50 md:left-0 md:top-1/2 md:-translate-y-1/2 md:h-auto md:w-[20px]">
         <div
           onClick={() => setIsLeftNeonBulbLit(!isLeftNeonBulbLit)}
-          className={`h-full w-[200px] cursor-pointer rounded-b-full sm:h-[300px] sm:w-full sm:rounded-b-none sm:rounded-r-full
+          className={`h-full w-[200px] cursor-pointer rounded-b-full md:h-[300px] md:w-full md:rounded-b-none md:rounded-r-full
             ${
               isLeftNeonBulbLit
                 ? "bg-primary-500 shadow-[0_0_40px_theme(colors.primary.600),0_0_120px_theme(colors.primary.700)] hover:bg-primary-400"
--- src/components/right-neon-bulb.tsx
+++ src/components/right-neon-bulb.tsx
@@ -7,10 +7,10 @@ export default function RightNeonBulb() {
 
   return (
     <div>
-      <div className="fixed inset-x-0 bottom-0 flex h-[20px] w-full justify-center z-50 sm:fixed sm:right-0 sm:left-auto sm:top-0 sm:bottom-0 sm:h-full sm:w-[20px] sm:items-center">
+      <div className="fixed inset-x-0 bottom-0 flex h-[20px] w-full justify-center z-50 md:right-0 md:left-auto md:top-0 md:bottom-0 md:h-full md:w-[20px] md:items-center">
         <div
           onClick={() => setIsRightNeonBulbLit(!isRightNeonBulbLit)}
-          className={`h-full w-[200px] cursor-pointer rounded-t-full sm:h-[300px] sm:w-full sm:rounded-none sm:rounded-tl-full sm:rounded-bl-full
+          className={`h-full w-[200px] cursor-pointer rounded-t-full md:h-[300px] md:w-full md:rounded-none md:rounded-tl-full md:rounded-bl-full
             ${
               isRightNeonBulbLit
                 ? "bg-primary-500 shadow-[0_0_40px_theme(colors.primary.600),0_0_120px_theme(colors.primary.700)] hover:bg-primary-400"
